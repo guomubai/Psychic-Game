a8fb3bf8f46d0a9daf0372ed1071f9c91c4d9f86
Guomubai <erickjgarcia@gmail.com>
2018-08-13 16:42:49 -0600

    an update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,55 @@
 // First, let's set out a variable for the list of words that we need - it's gonna be an array
-var wordbank = ["Doug" , "Ren" , "Stimpy" , "Rugrats" , "Rocko", "Tommy", "Chuckie", "Porkchop", "Skeeter"]
-
+var wordbank = ["Doug", "Ren", "Stimpy", "Rugrats", "Rocko", "Tommy", "Chuckie", "Porkchop", "Skeeter"]
 // Now, the computer will choose a random word from the wordbank, and set it to the variable "word"
 var word = wordbank[Math.floor(Math.random() * wordbank.length)]
-
 // Then, I console log the word to make sure the computer guesses one. This is also how to enable "cheat mode" :o
-console.log(word);
+console.log("Computer picks as a word: " + word);
 
-// Next thing we do is make the list of "_" for the gameboard
-var lines = [];
+// Let's set oout the maximum amount of tries a player can get. This is basically the buildup to the drawing of the hangman. Once the player uses up ten tries, they lose the game
+var maxTries = 10;
+// Number of wins
+var wins = 0;
+// Number of losses
+var loss = 0;
+// to keep track of the letters you typed
+var guessedLetters = [];
+// Let's create the empty array that will hold the "_"
+var answerArray = [];
+// Now we will look through this to create the lines that ween need
 for (i = 0; i < word.length; i++) {
-	lines[i] - "_";
-	// log it as well to seee if the lines are coming through
-	console.log(lines)
+	answerArray[i] = "_";
+}
+// log it out to check - I did and it wors. I origianlly had this as answerArray[i] but obviously that does not work becasue it would be logging only 
+console.log("Here is the array" + answerArray);
+
+// then we set out the remining letters so that we keep track of how many letters are left to be guessed. It will go down by 1 for each instant of that letter in the word
+var remainingLetters = word.length;
+
+// setting out the variable for the user guess
+var userGuess;
+
+// when you press a key. . .
+document.onkeyup = function (event) {
+	var userGuess = event.key;
+	// log that key to make sure
+	console.log(userGuess)
+
+// I am trying the below out, so iff 
+
+	for (var j = 0; j < word.length; j++) {
+		if (word[j] === userGuess) {
+			answerArray[j] = userGuess;
+			remainingLetters--;
+			conmsole.log(word);
+			console.log(answerArray)
+		}
+	}
 }
 
-// Then we set out the variable that will tell us how many letters are left on on the word they are guesses. We know that whatever it is, it is the length of the word that was picked
-var lettersLeft = word.length;
+// // testing to see if this works --- it did not, so I commented it out
+// if (remainingLetters > 0) {
+	
+// // testing here
+// alert(answerArray.join(" "));
 
-
+// }
